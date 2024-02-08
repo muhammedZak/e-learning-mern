2fe24ddfc5fd9baf567833af2dc9b203d4096c11
muhammedZak <zakariya.zaku018@gmail.com>
2024-02-09 01:03:44 +0530

    Initial commit

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import MainSlider from '../../components/Slider/MainSlider/MainSlider';
+import Container from '../../components/Layouts/Container';
 
 const HomePage = () => {
   return (
@@ -9,6 +10,22 @@ const HomePage = () => {
         <Navbar />
       </header>
       <MainSlider />
+      <div>
+        <Container>
+          <h3 className="py-3 text-4xl text-slate-900  font-extrabold">
+            Popular Topics
+          </h3>
+          <p className="text-xl">
+            Choose from over 210,000 online video courses with new additions
+            published every month
+          </p>
+          <div>
+            <div>
+              <img src="" alt="" />
+            </div>
+          </div>
+        </Container>
+      </div>
     </>
   );
 };
