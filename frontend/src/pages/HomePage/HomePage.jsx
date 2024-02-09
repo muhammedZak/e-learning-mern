f19444ca0c3bbb566bb37c909f7a8775e3d7cfe1
muhammedZak <zakariya.zaku018@gmail.com>
2024-02-10 00:47:08 +0530

    Added Multiple Sections

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -3,6 +3,10 @@ import Navbar from '../../components/Navbar/Navbar';
 import MainSlider from '../../components/Slider/MainSlider/MainSlider';
 import Container from '../../components/Layouts/Container';
 import CategorySlider from '../../components/Slider/CategorySlider/CategorySlider';
+import CategoryLists from '../../components/CategoryLists/CategoryLists';
+import CourseListContainer from '../../components/CourseListContainer/CourseListContainer';
+import InstructorSection from '../../components/InstructorSection/InstructorSection';
+import Testimonial from '../../components/Testimonial/Testimonial';
 
 const HomePage = () => {
   return (
@@ -22,44 +26,19 @@ const HomePage = () => {
           </p>
           {/* <CategorySlider /> */}
           <div className="py-5">
-            <div className="flex gap-7 flex-nowrap overflow-y-scroll no-scrollbar">
-              <div className="shrink-0	">
-                <button className="text-lg text-slate-900 font-medium">
-                  React
-                </button>
-              </div>
-              <div className="shrink-0	">
-                <button className="text-lg text-slate-900 font-medium">
-                  Javascript
-                </button>
-              </div>
-              <div className="shrink-0	">
-                <button className="text-lg text-slate-900 font-medium">
-                  Python
-                </button>
-              </div>
-              <div className="shrink-0	">
-                <button className="text-lg text-slate-900 font-medium">
-                  Web Development
-                </button>
-              </div>
-              <div className="shrink-0	">
-                <button className="text-lg text-slate-900 font-medium">
-                  Data Science
-                </button>
-              </div>
-              <div className="shrink-0	">
-                <button className="text-lg text-slate-900 font-medium">
-                  Excel
-                </button>
-              </div>
-              <div className="shrink-0	">
-                <button className="text-lg text-slate-900 font-medium">
-                  Amazon AWS
-                </button>
-              </div>
-            </div>
+            <CategoryLists />
+            <CourseListContainer />
           </div>
+        </Container>
+      </div>
+      <div>
+        <Container>
+          <InstructorSection />
+        </Container>
+      </div>
+      <div>
+        <Container>
+          <Testimonial />
         </Container>
       </div>
     </>
