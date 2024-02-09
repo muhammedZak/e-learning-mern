2091abd6094e6ff79019cc7d36372d17036f07b5
muhammedZak <zakariya.zaku018@gmail.com>
2024-02-10 00:46:53 +0530

    Changed Some Styles

--- frontend/src/components/Layouts/Container.jsx
+++ frontend/src/components/Layouts/Container.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Container = ({ children }) => {
-  return <div className="px-6 py-10">{children}</div>;
+  return <div className="px-6 py-8">{children}</div>;
 };
 
 export default Container;
