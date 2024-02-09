1239e07f1f83d0608ca2696f4a92fd1e0413e905
muhammedZak <zakariya.zaku018@gmail.com>
2024-02-10 00:45:58 +0530

    Created Course List Container

---
+++ frontend/src/components/CourseListContainer/CourseListContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from '../Card/Card';
+
+const CourseListContainer = () => {
+  return (
+    <div className="py-8 flex gap-4">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
+
+export default CourseListContainer;
