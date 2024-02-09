b5926df510a5a3b31322e5a6a823a66d7fcf981a
muhammedZak <zakariya.zaku018@gmail.com>
2024-02-09 15:47:10 +0530

    Added some changes to components

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import MainSlider from '../../components/Slider/MainSlider/MainSlider';
 import Container from '../../components/Layouts/Container';
+import CategorySlider from '../../components/Slider/CategorySlider/CategorySlider';
 
 const HomePage = () => {
   return (
@@ -19,9 +20,44 @@ const HomePage = () => {
             Choose from over 210,000 online video courses with new additions
             published every month
           </p>
-          <div>
-            <div>
-              <img src="" alt="" />
+          {/* <CategorySlider /> */}
+          <div className="py-5">
+            <div className="flex gap-7 flex-nowrap overflow-y-scroll no-scrollbar">
+              <div className="shrink-0	">
+                <button className="text-lg text-slate-900 font-medium">
+                  React
+                </button>
+              </div>
+              <div className="shrink-0	">
+                <button className="text-lg text-slate-900 font-medium">
+                  Javascript
+                </button>
+              </div>
+              <div className="shrink-0	">
+                <button className="text-lg text-slate-900 font-medium">
+                  Python
+                </button>
+              </div>
+              <div className="shrink-0	">
+                <button className="text-lg text-slate-900 font-medium">
+                  Web Development
+                </button>
+              </div>
+              <div className="shrink-0	">
+                <button className="text-lg text-slate-900 font-medium">
+                  Data Science
+                </button>
+              </div>
+              <div className="shrink-0	">
+                <button className="text-lg text-slate-900 font-medium">
+                  Excel
+                </button>
+              </div>
+              <div className="shrink-0	">
+                <button className="text-lg text-slate-900 font-medium">
+                  Amazon AWS
+                </button>
+              </div>
             </div>
           </div>
         </Container>
