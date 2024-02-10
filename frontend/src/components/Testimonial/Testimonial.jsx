c13ef2e01100308555159b9a2da1d724c4ef90e4
muhammedZak <zakariya.zaku018@gmail.com>
2024-02-10 15:16:24 +0530

    Created Testimonials

--- frontend/src/components/Testimonial/Testimonial.jsx
+++ frontend/src/components/Testimonial/Testimonial.jsx
@@ -1,17 +1,62 @@
-import React from 'react';
 import TestimonialItem from './TestimonialItem';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { NextBtn, PreviousBtn } from '../Slider/MainSlider/MainSlider';
 
 const Testimonial = () => {
+  const isMobile = window.innerWidth <= 1024;
+
+  const settings = {
+    className: 'center',
+    centerMode: true,
+    infinite: true,
+    // centerPadding: '60px',
+    slidesToShow: 3,
+    speed: 500,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 920,
+        settings: {
+          centerMode: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          centerMode: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+    prevArrow: <PreviousBtn className="testimonial" />,
+    nextArrow: <NextBtn className="testimonial" />,
+  };
+
   return (
     <div>
       <h1 className="pb-3 text-4xl text-center font-medium">Testimonials</h1>
       <div className="flex justify-center">
         <div className="w-28 h-1 mb-5 bg-black"></div>
       </div>
-      <div className="py-4 px-20 flex justify-center gap-9">
-        <TestimonialItem />
-        <TestimonialItem />
-        <TestimonialItem />
+
+      <div className="testimonial">
+        <Slider {...settings}>
+          <TestimonialItem />
+          <TestimonialItem />
+          <TestimonialItem />
+        </Slider>
       </div>
     </div>
   );
--- frontend/src/components/Testimonial/TestimonialItem.jsx
+++ frontend/src/components/Testimonial/TestimonialItem.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const TestimonialItem = () => {
+const TestimonialItem = ({ isMobile }) => {
   return (
-    <div className="">
+    <div className="md:mx-2">
       <div className="flex justify-center -mb-8  ">
         <div className="w-20 p-1 bg-white rounded-full">
           <img
@@ -13,15 +13,14 @@ const TestimonialItem = () => {
         </div>
       </div>
       <div className="py-7 px-10 flex items-center flex-col bg-slate-100">
-        <div className="py-3 text-center ">
-          <p>
+        <div className="py-3  shrink-0 ">
+          <p className="">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint,
             soluta excepturi facere eaque id, numquam, delectus dolores
-            asperiores error repellat expedita sunt quos voluptates. Ipsum, vel
-            possimus. Obcaecati, corporis! Quae.
+            asperiores error repellat expedita
           </p>
         </div>
-        <div>
+        <div className="text-end">
           <p>
             John Doe <span>CEO at Google</span>
           </p>
