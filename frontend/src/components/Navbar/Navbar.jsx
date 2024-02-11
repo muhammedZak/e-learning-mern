1c93fee8d216a1dd5a10b19da2ec557a8476a10d
muhammedZak <zakariya.zaku018@gmail.com>
2024-02-11 20:26:24 +0530

    Implimented custom hook

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './Navbar.css';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
@@ -5,14 +6,16 @@ import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import CloseIcon from '@mui/icons-material/Close';
 
-import { useState } from 'react';
 import Sidenav from '../Sidenav/Sidenav';
 import NavbarDropdown from '../NavbarDropdown/NavbarDropdown';
 import LoginSignupButton from '../Buttons/LoginSignupButton/LoginSignupButton';
 import NavbarSearch from '../NavbarSearchBar/NavbarSearch';
 import NavbarIconButtons from '../Buttons/NavbarIconButtons/NavbarIconButtons';
+import useScreenSize from '../../customHooks/useScreenSize';
 
 const Navbar = () => {
+  const { width: screenWidth } = useScreenSize();
+  const [isMobile, setIsMobile] = useState(false);
   const [open, setOpen] = useState(false);
   const [showNestedSidebar, setShowNestedSidebar] = useState(false);
   const [showCategoryDropdown, setShowCategoryDropdown] = useState(false);
@@ -42,26 +45,34 @@ const Navbar = () => {
     setShowNestedSidebar(false);
   };
 
+  useEffect(() => {
+    setIsMobile(screenWidth < 800);
+  }, [screenWidth]);
+
   return (
     <>
-      {(open || showNestedSidebar) && (
-        <div className="overlay" onClick={handleClose}></div>
+      {(open || showNestedSidebar) && isMobile && (
+        <>
+          <div className="overlay" onClick={handleClose}></div>
+          <button
+            onClick={handleClose}
+            className={`${
+              open ? 'absolute' : 'hidden'
+            } p-3 z-10 left-80 top-5   rounded-full bg-slate-300 transition-colors hover:bg-slate-200`}
+          >
+            <CloseIcon />
+          </button>
+        </>
       )}
-      <button
-        onClick={handleClose}
-        className={`${
-          open ? 'absolute' : 'hidden'
-        } p-3 z-10 left-80 top-5   rounded-full bg-slate-300 transition-colors hover:bg-slate-200`}
-      >
-        <CloseIcon />
-      </button>
-      <Sidenav
-        open={open}
-        onClose={handleClose}
-        nestedSidebarOpen={showNestedSidebar}
-        nestedSidebarHandle={handleNestedSidebar}
-        nestedSidebarClose={handleNestedSidebarClose}
-      />
+      {isMobile && (
+        <Sidenav
+          open={open}
+          onClose={handleClose}
+          nestedSidebarOpen={showNestedSidebar}
+          nestedSidebarHandle={handleNestedSidebar}
+          nestedSidebarClose={handleNestedSidebarClose}
+        />
+      )}
       <nav className="px-6 md:py-2 h-16 flex items-center justify-between md:justify-normal bg-slate-100 border-b-2">
         <div className="md:hidden">
           <IconButton onClick={handleDrawerOpen}>
@@ -75,20 +86,24 @@ const Navbar = () => {
             alt="logo"
           />
         </div>
-        <div>
-          <div
-            onMouseEnter={onCategoryMouseEnter}
-            onMouseLeave={onCategoryMouseLeave}
-            className="relative"
-          >
-            <button className="hidden mx-4  md:block  cursor-auto  text-slate-900 text-sm font-medium">
-              Categories
-            </button>
-            <NavbarDropdown show={showCategoryDropdown} />
-          </div>
-        </div>
+        {!isMobile && (
+          <>
+            <div>
+              <div
+                onMouseEnter={onCategoryMouseEnter}
+                onMouseLeave={onCategoryMouseLeave}
+                className="relative"
+              >
+                <button className="hidden mx-4  md:block  cursor-auto  text-slate-900 text-sm font-medium">
+                  Categories
+                </button>
+                <NavbarDropdown show={showCategoryDropdown} />
+              </div>
+            </div>
+            <NavbarSearch />
+          </>
+        )}
 
-        <NavbarSearch />
         <div className="mr-10 hidden lg:block  text-slate-900 text-sm font-medium">
           Teach on Udemy
         </div>
