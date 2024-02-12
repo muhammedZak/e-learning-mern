da2b0fbd3d0f527b978d652123281e58b6203ea1
muhammedZak <zakariya.zaku018@gmail.com>
2024-02-12 12:56:35 +0530

    Made courseList responsive

--- frontend/src/components/CourseListContainer/CourseListContainer.jsx
+++ frontend/src/components/CourseListContainer/CourseListContainer.jsx
@@ -1,14 +1,67 @@
-import React from 'react';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
+import { NextBtn, PreviousBtn } from '../Slider/MainSlider/MainSlider';
 import Card from '../Card/Card';
 
 const CourseListContainer = () => {
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    prevArrow: <PreviousBtn className="courseList" />,
+    nextArrow: <NextBtn className="courseList" />,
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 860,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 630,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
-    <div className="py-8 flex gap-4">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+    <div className="courseList  pt-5">
+      <Slider {...settings} className="px-6">
+        <div>
+          <Card />
+        </div>
+        <div>
+          <Card />
+        </div>
+        <div>
+          <Card />
+        </div>
+        <div>
+          <Card />
+        </div>
+        <div>
+          <Card />
+        </div>
+      </Slider>
     </div>
   );
 };
