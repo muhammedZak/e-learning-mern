dcbb35cedee98d733548f267e35c5c5e4ed9e090
muhammedZak <zakariya.zaku018@gmail.com>
2024-02-19 09:54:37 +0530

    implimented react-hook form

--- frontend/src/components/NavbarSearchBar/NavbarSearch.jsx
+++ frontend/src/components/NavbarSearchBar/NavbarSearch.jsx
@@ -4,12 +4,17 @@ const NavbarSearch = () => {
   return (
     <div className="hidden md:block flex-1 ml-6">
       <div className="border-2 w-60 flex  rounded-sm border-slate-500 p-1">
-        <input
-          className="outline-none flex-1 bg-inherit"
-          type="search"
-          placeholder="Search here"
-        />
-        <SearchOutlinedIcon className="text-slate-900" />
+        <form action="">
+          <input
+            autoComplete="false"
+            name="search"
+            id="search"
+            className="outline-none flex-1 bg-inherit"
+            type="search"
+            placeholder="Search here"
+          />
+          <SearchOutlinedIcon className="text-slate-900" />
+        </form>
       </div>
     </div>
   );
--- frontend/src/pages/Auth/SignUpPage.jsx
+++ frontend/src/pages/Auth/SignUpPage.jsx
@@ -1,43 +1,98 @@
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 const SignUpPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <div className="h-screen flex  justify-center">
-      <div className="pt-16">
+    <div className="h-screen flex items-center justify-center">
+      <div className="">
         <h4 className="font-semibold text-2xl">Sign up here</h4>
         <hr className="my-2 " />
-        <form action="">
-          <label className="mb-1 text-lg text-slate-900 font-medium block">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label
+            htmlFor="name"
+            className="mb-1 text-lg text-slate-900 font-medium block"
+          >
             Name
           </label>
           <input
+            id="name"
+            name="name"
             type="text"
             placeholder="John Doe"
-            required
+            {...register('name', { required: true, minLength: 4 })}
             className="mb-4 py-3 pl-3 w-full border border-black"
           />
-          <label className="mb-1 text-lg text-slate-900 font-medium block">
+          {errors.name?.type === 'required' && (
+            <p className="text-red-500" role="alert">
+              * Name is required
+            </p>
+          )}
+          <label
+            htmlFor="email"
+            className="mb-1 text-lg text-slate-900 font-medium block"
+          >
             Email
           </label>
           <input
+            id="email"
+            name="email"
             type="email"
             placeholder="johndoe@gmail.com"
-            required
+            {...register('email', {
+              required: true,
+              pattern:
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            })}
             className="mb-4 py-3 pl-3 w-full border border-black"
           />
-          <label className="mb-1 text-lg text-slate-900 font-medium block">
+          {errors.email?.type === 'required' && (
+            <p className="text-red-500" role="alert">
+              * Email is required
+            </p>
+          )}
+          <label
+            htmlFor="password"
+            className="mb-1 text-lg text-slate-900 font-medium block"
+          >
             Password
           </label>
           <input
+            id="password"
+            name="password"
             type="password"
-            required
+            {...register('password', {
+              required: true,
+              minLength: 6,
+            })}
             className="mb-4 py-3 pl-3 w-full border border-black"
           />
-          <label htmlFor="" className="block mb-4 w-full text-sm">
-            <input type="checkbox" className="mr-1" />
+          {errors.password?.type === 'required' ? (
+            <p className="text-red-500" role="alert">
+              * Password is required
+            </p>
+          ) : errors.password?.type === 'minLength' ? (
+            <p className="text-red-500" role="alert">
+              * Minimum 6 charectors is required
+            </p>
+          ) : (
+            ''
+          )}
+          <label htmlFor="terms" className="block mb-4 w-full text-sm">
+            <input id="terms" type="checkbox" className="mr-1" />
             By signing up, you agree to our Terms of Use and Privacy Policy.
           </label>
-          <button className="py-3 px-32 block w-full text-center text-white bg-blue-500 hover:bg-opacity-75">
+          <button
+            type="submit"
+            className="py-3 px-32 block w-full text-center text-white bg-blue-500 hover:bg-opacity-75"
+          >
             Sign up
           </button>
         </form>
