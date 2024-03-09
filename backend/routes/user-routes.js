0e96cc82a2d3dbf9aac4ed1da05c2368b864b465
muhammedZak <zakariya.zaku018@gmail.com>
2024-03-10 02:28:11 +0530

    created user router

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -8,6 +8,9 @@ const {
   passwordChange,
   getMyProfile,
   updateProfile,
+  forgotPassword,
+  resetPassword,
+  otpRequest,
 } = require('../controllers/user-controller');
 
 const { isAdmin, protect } = require('../middlewares/auth-middleware');
@@ -17,6 +20,9 @@ const router = express.Router();
 router.post('/signup', signup);
 router.post('/signin', login);
 router.post('/logout', logout);
+router.post('/forgot-password', forgotPassword);
+router.put('/reset-password/:token', resetPassword);
+// router.post('/request-otp', otpRequest);
 
 router.use(protect);
 router.put('/email-edit', editEmail);
