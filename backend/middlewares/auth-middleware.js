5e3caaeadabeb6843e0096141e5537b5c7b0a52e
muhammedZak <zakariya.zaku018@gmail.com>
2024-03-10 02:25:58 +0530

    Created middleware function for checking course is created by the current user

--- backend/middlewares/auth-middleware.js
+++ backend/middlewares/auth-middleware.js
@@ -1,8 +1,10 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user-model');
 const catchAsync = require('../utils/catch-async');
+const Course = require('../models/course-modal');
 
 const protect = catchAsync(async (req, res, next) => {
+  console.log('protect--------->>>');
   let token;
   token = req.cookies.jwt;
 
@@ -25,6 +27,7 @@ const protect = catchAsync(async (req, res, next) => {
 });
 
 const isAdmin = (req, res, next) => {
+  console.log('isAdmin--------->>>');
   if (req.user && req.user.role === 'admin') {
     next();
   } else {
@@ -33,4 +36,41 @@ const isAdmin = (req, res, next) => {
   }
 };
 
-module.exports = { isAdmin, protect };
+const isTutor = (req, res, next) => {
+  console.log('isTutor--------->>>');
+  if (req.user && req.user.role === 'instructor') {
+    next();
+  } else {
+    res.status(401);
+    throw new Error('Not authorized as an instructor');
+  }
+};
+
+const isCourseByTutor = async (req, res, next) => {
+  console.log('courseBy/tutor--------->>>');
+  try {
+    const isInstructor = await Course.find({
+      instructor: req.user._id,
+      _id: req.params.courseId,
+    });
+
+    console.log('params:', req.params);
+    console.log('moduleId:', req.params.id);
+    console.log('courseId:', req.params.courseId);
+    console.log('userId:', req.user._id);
+    console.log('isInstructor:', isInstructor);
+
+    if (isInstructor && isInstructor.length > 0) {
+      next();
+    } else {
+      res.status(400);
+      throw new Error(
+        `You are not the owner of this course!. Pleae create course`
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { isAdmin, protect, isTutor, isCourseByTutor };
